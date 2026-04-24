b3cc82ce708514ed97ef9d5297c5dd1bd9d34e08
msbro-rmw <mdinabano189@gmail.com>
2026-04-24 20:10:54 +0530

    Update gen_session.py

--- utils/gen_session.py
+++ utils/gen_session.py
@@ -5,8 +5,8 @@ python utils/gen_session.py
 from pyrogram import Client
 import asyncio
 
-API_ID   = 0    # <-- bharo
-API_HASH = ""   # <-- bharo
+API_ID   = 38498066    # <-- bharo
+API_HASH = "c9696114751feacdeb1b4487f5839a1a"   # <-- bharo
 
 async def main():
     async with Client("gen_temp", api_id=API_ID, api_hash=API_HASH) as app:
